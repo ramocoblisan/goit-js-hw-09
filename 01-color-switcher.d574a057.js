e35f81a51b05571301b34637cd33948906d9459f
ramocoblisan <142051946+ramocoblisan@users.noreply.github.com>
2024-01-28 15:33:25 +0000

    Deploying to gh-pages from @ ramocoblisan/goit-js-hw-09@9be58044ee42dcdd774804d2df3cce0f1099a893 🚀

---
+++ 01-color-switcher.d574a057.js
@@ -0,0 +1,2 @@
+let t;const e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(t)}));
+//# sourceMappingURL=01-color-switcher.d574a057.js.map
